a278794f029018f0b1bba2e0e70a6fd78f70b6ef
mohit dubey <v-mohit.dubey@magicedtech.com>
2023-03-03 16:11:13 +0530

    adding todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,189 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { useState } from "react";
+import { v4 as idGen } from "uuid";
+import { MdDelete } from "react-icons/md";
+import { BiCheckbox, BiCheckboxChecked } from "react-icons/bi";
+import styles from "./App.module.css";
 
-function App() {
+const ToDoList = () => {
+  const [taskStr, setTaskStr] = useState("");
+  const [taskList, setTaskList] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  const saveLocal = (data) => {
+    localStorage.setItem("tasks", JSON.stringify(data));
+  };
+
+  const handleChange = (e) => {
+    setTaskStr(e.target.value);
+  };
+
+  const addTask = () => {
+    if (taskStr !== "") {
+      const taskDetails = {
+        id: idGen(),
+        value: taskStr,
+        isCompleted: false,
+        isEdit: false,
+      };
+
+      ref.current.value = "";
+      setTaskList([...taskList, taskDetails]);
+      saveLocal([...taskList, taskDetails]);
+    }
+    setTaskStr("");
+  };
+
+  const taskCompleted = (e, id) => {
+    e.preventDefault();
+    // find index of element
+    let temp = taskList;
+    const element = temp.findIndex((elem) => elem.id === id);
+    temp[element] = {
+      ...temp[element],
+      isCompleted: !temp[element].isCompleted,
+    };
+
+    setTaskList([...temp]);
+    saveLocal([...temp]);
+  };
+
+  const editTaskHandler = (e, id) => {
+    e.preventDefault();
+    let temp = taskList;
+    const element = temp.findIndex((elem) => elem.id === id);
+    temp[element] = {
+      ...temp[element],
+      isEdit: true,
+    };
+
+    setTaskList([...temp]);
+    saveLocal([...temp]);
+  };
+
+  const onEditing = (e, id) => {
+    e.preventDefault();
+
+    let temp = taskList;
+    const element = temp.findIndex((elem) => elem.id === id);
+    temp[element] = {
+      ...temp[element],
+      isEdit: true,
+      value: e.target.value,
+    };
+    setTaskList([...temp]);
+    saveLocal([...temp]);
+  };
+
+  const updateTaskHandler = (e, id) => {
+    e.preventDefault();
+
+    let temp = taskList;
+    const element = temp.findIndex((elem) => elem.id === id);
+    temp[element] = {
+      ...temp[element],
+      isEdit: false,
+    };
+    setTaskList([...temp]);
+    saveLocal([...temp]);
+  };
+
+  const filterRemaining = (e) => {
+    const newList = taskList.filter((t) => !t.isCompleted);
+    setTaskList(newList);
+    saveLocal(newList);
+  };
+
+  const deletetaskHandler = (e, id) => {
+    e.preventDefault();
+    const newList = taskList.filter((t) => t.id !== id);
+    setTaskList(newList);
+    saveLocal(newList);
+  };
+
+  const ref = useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>To Do List</h1>
+      <div className={styles.inputCss}>
+        <input
+          type="text"
+          ref={ref}
+          onChange={(e) => handleChange(e)}
+          placeholder="add a task"
+          className={styles.input}
+        />
+      </div>
+      <div>
+        <button className={styles.btnadd} onClick={addTask}>
+          Add
+        </button>
+        <button onClick={filterRemaining} className={styles.btnadd}>
+          Clear Completed
+        </button>
+      </div>
+      <div className={styles.listDiv}>
+        {taskList.map((item) => (
+          <li key={item.id} className={styles.list}>
+            <div className={styles.leftDiv}>
+              <BiCheckbox
+                size={30}
+                color="red"
+                onClick={(e) => {
+                  taskCompleted(e, item.id);
+                }}
+                className={item.isCompleted ? styles.invisible : styles.visible}
+              />
+              <BiCheckboxChecked
+                size={30}
+                color="red"
+                className={item.isCompleted ? styles.visible : styles.invisible}
+                onClick={(e) => {
+                  taskCompleted(e, item.id);
+                }}
+              />
+
+              {item.isEdit ? (
+                <input
+                  type="text"
+                  value={item.value}
+                  onChange={(e) => onEditing(e, item.id)}
+                  className={styles.editInput}
+                />
+              ) : (
+                <span
+                  onDoubleClick={(e) => {
+                    editTaskHandler(e, item.id);
+                  }}
+                >
+                  {" "}
+                  {item.value}
+                </span>
+              )}
+            </div>
+
+            {item.isEdit ? (
+              <button
+                className={styles.updatebtn}
+                onClick={(e) => updateTaskHandler(e, item.id)}
+              >
+                Update
+              </button>
+            ) : (
+              <div
+                className={styles.visible}
+                onClick={(e) => deletetaskHandler(e, item.id)}
+              >
+                <MdDelete color="red" size={25} />
+              </div>
+            )}
+          </li>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default ToDoList;
